0f9114c48b0c7dd30021a734e9ccf560a38165c4
Dev Kumar <devkr074@gmail.com>
2025-10-14 09:33:53 +0530

    SELECT

--- Program_1.sql
+++ Program_1.sql
@@ -1,13 +1,5 @@
-/*Database Creation, Deletion and Updation in SQL*/
-
-CREATE DATABASE myDB;	/*Create New Database*/
-
-SHOW DATABASES;		/*Displays all Databases*/
-
-USE myDB;	/*Use Created Database*/
-
-DROP DATABASE myDB;		/*Delete Database*/
-
-ALTER DATABASE myDB READ ONLY=1;	/*Alter Database to Read Mode You can't modify or delete database*/
-
-ALTER DATABASE myDB READ ONLY=0;	/*Alter Database to Read and Write Mode You can modify or delete database*/
+SELECT CustomerName, City, Country FROM Customers;
+SELECT * FROM Customers;
+SELECT Country FROM Customers;
+SELECT DISTINCT Country FROM Customers;
+SELECT COUNT(DISTINCT Country) FROM Customers;
