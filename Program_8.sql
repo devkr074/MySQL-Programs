07a8f15d9e160c73fdf578b355dcf9d04d433b5a
Dev Kumar <devkr074@gmail.com>
2025-10-15 09:34:27 +0530

    DELETE

--- Program_8.sql
+++ Program_8.sql
@@ -1,14 +1,2 @@
-/*Unique Contraint in SQL*/
-
-CREATE TABLE products(product_id INT UNIQUE,product_name VARCHAR(25),price DOUBLE(4,2));
-
-ALTER TABLE products ADD CONSTRAINT UNIQUE(product_name);
-
-INSERT INTO products VALUES (100,"Hamburger",3.99),
-							(101,"Fries",1.89),
-                            (102,"Soda",1.00),
-                            (103,"Ice Cream",1.49);
-
-SELECT * FROM products;
-
-DESC products;
+DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';
+DELETE FROM Customers;
