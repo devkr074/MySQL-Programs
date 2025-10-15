8b5ff080348c050a02c7ff5ad2c9d56a23239f0c
Dev Kumar <devkr074@gmail.com>
2025-10-15 09:31:42 +0530

    INSERT INTO

--- Program_5.sql
+++ Program_5.sql
@@ -1,13 +1,4 @@
-/*Update and Delete Row in SQL*/
-
-UPDATE employees SET hourly_pay=10.25 WHERE employee_id=6;		/*Updates Single Column Data of a Row*/
-
-UPDATE employees SET hourly_pay=10.50,hire_date="2023-05-30" WHERE employee_id=6;		/*Updates Multiple Columns Data of a Row*/
-
-UPDATE employees SET hire_date=NULL WHERE employee_id=6;		/*Updates value to NULL*/
-
-DELETE FROM employees WHERE employee_id=6;		/*Deletes Particular Row of a Table*/
-
-SELECT * FROM employees;
-
-/*Note: If we don't use where clause in update and delete query it will update and delete all the rows of table*/
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Cardinal', 'Stavanger', 'Norway');
