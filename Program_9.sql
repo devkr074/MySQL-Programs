1e564356fd18ccb9f9fe6d49df3f3990b7935f7e
Dev Kumar <devkr074@gmail.com>
2025-10-16 22:28:26 +0530

    LIMIT

--- Program_9.sql
+++ Program_9.sql
@@ -1,13 +1,10 @@
-/*NOT NULL Constraint in SQL*/
-
-CREATE TABLE products(product_id INT,product_name VARCHAR(25),price DOUBLE(4,2) NOT NULL);		/*If creating new table*/
-
-ALTER TABLE products MODIFY price DECIMAL(4,2) NOT NULL;		/*Add NOT NULL Constraint to column of already existing table*/
-
-INSERT INTO products VALUES (104,"Cookie",NULL);		/*Error because price value cannot be NULL*/
-
-INSERT INTO products VALUES (104,"Cookie",0);
-
-SELECT * FROM products;
-
-DESC products;
+SELECT * FROM Customers
+LIMIT 3;
+SELECT * FROM Customers
+LIMIT 3 OFFSET 3;
+SELECT * FROM Customers
+WHERE Country='Germany'
+LIMIT 3;
+SELECT * FROM Customers
+ORDER BY Country
+LIMIT 3;
