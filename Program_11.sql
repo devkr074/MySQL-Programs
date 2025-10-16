47c91ab5e4dea8ab6cf97f069ae09e0ee285d4cd
Dev Kumar <devkr074@gmail.com>
2025-10-16 22:32:52 +0530

    COUNT, AVG, SUM

--- Program_11.sql
+++ Program_11.sql
@@ -1,14 +1,6 @@
-/*Default Constraint in SQL*/
-
-CREATE TABLE products (product_id INT,product_name VARCHAR(25),price DOUBLE(4,2) DEFAULT 0);		/*Default Contreaint in New Table*/
-
-ALTER TABLE products ALTER price SET DEFAULT 0;		/*Default Constraint in Already Existing Table*/
-
-DESC products;
-
-INSERT INTO products(product_id,product_name) VALUES (104,"Straws"),
-													 (105,"Napkins"),
-                                                     (106,"Fork"),
-                                                     (107,"Spoon");
-
-SELECT * FROM products;
+SELECT COUNT(ProductID)
+FROM Products;
+SELECT AVG(Price)
+FROM Products;
+SELECT SUM(Quantity)
+FROM OrderDetails;
