aa544da495b31c3e66930809d2a3e6bcee5b3546
Dev Kumar <devkr074@gmail.com>
2025-10-16 22:34:24 +0530

    LIKE

--- Program_12.sql
+++ Program_12.sql
@@ -1,15 +1,14 @@
-/*Primary Key in SQL*/
-
-/*Primary Key: Primary Key is a column by which data is identified uniquelly. It is unique and not null. There could only one primary key in a table.*/
-
-CREATE TABLE transactions(transaction_is INT PRIMARY KEY,amount DECIMAL(5,2));		/*Primary Key in New Table*/
-
-ALTER TABLE transactions ADD CONSTRAINT PRIMARY KEY(transaction_id);		/*Primary Key in already existing table*/
-
-DESC transactions;
-
-INSERT INTO transactions VALUES (1000,4.99),
-							    (1001,2.89),
-                                (1002,3.95);
-                               
-SELECT * FROM transactions;
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a%';
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%a';
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%or%';
+SELECT * FROM Customers
+WHERE CustomerName LIKE '_r%';
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a__%';
+SELECT * FROM Customers
+WHERE ContactName LIKE 'a%o';
+SELECT * FROM Customers
+WHERE CustomerName NOT LIKE 'a%';
