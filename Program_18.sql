3bf44fa2aaa3bf46215d96fa849f4b0578e2b2d9
Dev Kumar <devkr074@gmail.com>
2025-10-18 01:14:59 +0530

    INNER JOIN

--- Program_18.sql
+++ Program_18.sql
@@ -1,13 +1,7 @@
-/*Wild Cards Character in SQL*/
-
-SELECT * FROM employees WHERE first_name LIKE "s%";		/*Return employees whose first character of first name is s*/
-
-SELECT * FROM employees WHERE first_name LIKE "sp%";
-
-SELECT * FROM employees WHERE last_name LIKE "%r";		/*Return employees whose last character of last name is r*/
-
-SELECT * FROM employees WHERE first_name LIKE "_pon%";		/*_ means one single character*/
-
-SELECT * FROM employees WHERE first_name LIKE "__on%";		/*Here two underscores so it will check string after two characters*/
-
-SELECT * FROM employees WHERE first_name LIKE "__o_%";
+SELECT Orders.OrderID, Customers.CustomerName
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
