78f491b3caff5f8730652e7fa26a9110c07ab35a
Dev Kumar <devkr074@gmail.com>
2025-10-17 09:29:49 +0530

    Wildcards

--- Program_13.sql
+++ Program_13.sql
@@ -1,15 +1,8 @@
-/*Auto Increment in SQL*/
-
-/*Auto Increment: Auto Increment is used with column that is set as Primary Key in a table. The Auto Increment Column will automatically inserts value by incrementing by 1. By default auto increment start from 1*/
-
-CREATE TABLE transactions(transaction_is INT PRIMARY KEY AUTO_INCREMENT,amount DECIMAL(5,2));
-
-DESC transactions;
-
-INSERT INTO transactions(amount) VALUES (4.99),
-										(2.89),
-										(3.95);
-                               
-ALTER TABLE transactions AUTO_INCREMENT=1000;		/*Changes starting value of auto increment from 1 to 1000. Now auto increment will start from 1000.*/
-
-SELECT * FROM transactions;
+SELECT * FROM Customers
+WHERE City LIKE 'ber%';
+SELECT * FROM Customers
+WHERE City LIKE '%es%';
+SELECT * FROM Customers
+WHERE City LIKE '_ondon';
+SELECT * FROM Customers
+WHERE City LIKE 'L_n_on';
