cbe4d4510832813315ec16724baa1302ee0f5567
Dev Kumar <devkr074@gmail.com>
2025-10-17 09:33:22 +0530

    Aliases

--- Program_16.sql
+++ Program_16.sql
@@ -1,13 +1,12 @@
-/*Functions in SQL*/
-
-SELECT COUNT(amount) AS Number_of_Transactions FROM transactions;		/*Return Count of number of rows in column*/
-
-SELECT MAX(amount) AS Maximum_Amount FROM transactions;		/*Returns Maximum value*/
-
-SELECT MIN(amount) AS Minimum_Amount FROM transactions;		/*Returns Minimum value*/
-
-SELECT AVG(amount) AS Average_Amount FROM transactions;		/*Returns Avearge*/
-
-SELECT SUM(amount) AS Total_Amount FROM transactions;		/*Returns Sum*/
-
-SELECT CONCAT(first_name," ",last_name) AS Full_Name FROM employees;		/*Concats Strings*/
+SELECT CustomerID AS ID, CustomerName AS Customer
+FROM Customers;
+SELECT CustomerName AS Customer, ContactName AS "Contact Person"
+FROM Customers;
+SELECT CustomerName, CONCAT_WS(', ', Address, PostalCode, City, Country) AS Address
+FROM Customers;
+SELECT o.OrderID, o.OrderDate, c.CustomerName
+FROM Customers AS c, Orders AS o
+WHERE c.CustomerName='Around the Horn' AND c.CustomerID=o.CustomerID;
+SELECT Orders.OrderID, Orders.OrderDate, Customers.CustomerName
+FROM Customers, Orders
+WHERE Customers.CustomerName='Around the Horn' AND Customers.CustomerID=Orders.CustomerID;
