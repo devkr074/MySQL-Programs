20d5deb876241cbc34974abc63909b7a1b587c25
Dev Kumar <devkr074@gmail.com>
2025-10-18 01:16:47 +0530

    RIGHT JOIN

--- Program_20.sql
+++ Program_20.sql
@@ -1,11 +1,4 @@
-/*Limit in SQL*/
-
-SELECT * FROM employees LIMIT 1;
-
-SELECT * FROM employees LIMIT 3;
-
-SELECT * FROM employees ORDER BY last_name LIMIT 1;
-
-SELECT * FROM employees ORDER BY last_name DESC LIMIT 1;
-
-SELECT * FROM employees LIMIT 2,2;		/*In this First number is offset it will leave 2 (1,2) data from data from top and display 2 (3,4) data after that*/
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
