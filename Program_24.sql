9aa2d95741cf91efea841742df043f583ab2e54f
Dev Kumar <devkr074@gmail.com>
2025-10-19 00:05:42 +0530

    UNION ALL

--- Program_24.sql
+++ Program_24.sql
@@ -1,15 +1,10 @@
-/*Indexes in SQL*/
-
-/*Indexes: Indexes are BTree Data Structure that makes searching of data more fast. UPDATE takes more time, SELECT takes less time.*/
-
-SHOW INDEXES FROM customers;		/*Displays all Indexes of a table*/
-
-CREATE INDEX last_name_idx ON customers(last_name);		/*Add New Indexes to table*/
-
-CREATE INDEX last_name_first_name_idx ON customers(last_name,first_name);		/*Add Multi Column Indexe*/
-
-ALTER TABLE customers DROP INDEX last_name_idx;		/*Delete Index from table*/
-
-SELECT * FROM customers WHERE last_name="Bass" AND first_name="Bubble";
-
-SELECT * FROM customers WHERE first_name="Bubble";
+SELECT City FROM Customers
+UNION ALL
+SELECT City FROM Suppliers
+ORDER BY City;
+SELECT City, Country FROM Customers
+WHERE Country='Germany'
+UNION ALL
+SELECT City, Country FROM Suppliers
+WHERE Country='Germany'
+ORDER BY City;
