cb3aeb6cb90714e1b669f4c48400d58fe6445ecd
Dev Kumar <devkr074@gmail.com>
2025-10-20 10:58:26 +0530

    GROUP BY

--- Program_25.sql
+++ Program_25.sql
@@ -1,7 +1,10 @@
-/*Subquery in SQL*/
-
-/*Subquery: Subquery is a query within another query.*/
-
-SELECT first_name,last_name,hourly_pay, (SELECT AVG(hourly_pay) FROM employees) AS Average_Pay FROM employees;
-
-SELECT first_name,last_name,hourly_pay FROM employees WHERE hourly_pay > (SELECT AVG(hourly_pay) FROM employees);
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country;
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+ORDER BY COUNT(CustomerID) DESC;
+SELECT Shippers.ShipperName, COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
+LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
+GROUP BY ShipperName;
