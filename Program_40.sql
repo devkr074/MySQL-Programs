4fe5ebe65f11efb064688f1d90fd36360e5ae6ed
Dev Kumar <devkr074@gmail.com>
2025-10-23 00:03:43 +0530

    Primary Key

---
+++ Program_40.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    PRIMARY KEY (ID)
+);
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    CONSTRAINT PK_Person PRIMARY KEY (ID,LastName)
+);
+ALTER TABLE Persons
+ADD PRIMARY KEY (ID);
+ALTER TABLE Persons
+ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName);
+ALTER TABLE Persons
+DROP PRIMARY KEY;
