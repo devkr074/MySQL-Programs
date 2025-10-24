d40185e3dec85261828e49c6d968f588d06d5fea
Dev Kumar <devkr074@gmail.com>
2025-10-24 21:56:14 +0530

    Default

---
+++ Program_43.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    City varchar(255) DEFAULT 'Sandnes'
+);
+CREATE TABLE Orders (
+    ID int NOT NULL,
+    OrderNumber int NOT NULL,
+    OrderDate date DEFAULT CURRENT_DATE()
+);
+ALTER TABLE Persons
+ALTER City SET DEFAULT 'Sandnes';
+ALTER TABLE Persons
+ALTER City DROP DEFAULT;
